749db94957a7b77be8b6417afa6c0fd392ea828c
sai3411 <dhulipallasaikiran@gmail.com>
2023-05-01 20:00:39 -0500

    New dataset

--- Crop.py
+++ Crop.py
@@ -1,22 +1,20 @@
-import matplotlib.pyplot as plt
 from PIL import Image
 import os
 
-data_path = 'dataset/'
+data_path = 'crop/'
 
 for dir in os.listdir(data_path):
-    for classes in os.listdir(data_path+'/'+ dir):
-        for file in os.listdir(data_path+'/'+dir+'/'+classes):
-            fmt = file.split('.')
+    for file in os.listdir(data_path+'/'+dir):
+        fmt = file.split('.')
 
-            if fmt[-1] == "jpg":
-                img_path = data_path+'/'+dir+'/'+classes+'/'+file
-                img = Image.open(img_path)
-                width, height = img.size
-                top_crop = 66
-                bottom_crop = 20
-                left_crop = 43
-                right_crop = 43 
-                box = (left_crop, top_crop, width-right_crop, height - bottom_crop)
-                cropped_img = img.crop(box)
-                cropped_img.save(img_path)
+        if fmt[-1] == "jpg":
+            img_path = data_path+'/'+dir+'/'+file
+            img = Image.open(img_path)
+            width, height = img.size
+            top_crop = 66
+            bottom_crop = 20
+            left_crop = 43
+            right_crop = 43 
+            box = (left_crop, top_crop, width-right_crop, height - bottom_crop)
+            cropped_img = img.crop(box)
+            cropped_img.save(img_path)
